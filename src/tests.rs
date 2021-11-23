bb1d2d665d5bd54f769a787f7f9fd4aecb2c408d
Harry <hello@majored.pw>
2021-11-23 23:47:37 +0000

    Addition of two tests & renaming of test methods

--- src/tests.rs
+++ src/tests.rs
@@ -4,18 +4,49 @@
 use crate::*;
 
 #[test]
-fn empty_builder() {
+fn empty() {
     let builder = RouterBuilder::<()>::new();
     assert!(builder.build().is_err());
 }
 
 #[test]
-fn singular_string() {
+fn singular() {
     let mut builder = RouterBuilder::<&str>::new();
     route!(builder; r"^/test$";; "GET" => "Hello.");
     let router = builder.build().unwrap();
 
-    assert!(router.dispatch("GET", "/test").is_some());
+    assert_eq!(*router.dispatch("GET", "/test").unwrap().handler(), "Hello.");
     assert!(router.dispatch("POST", "/test").is_none());
     assert!(router.dispatch("GET", "/test-test").is_none());
+}
+
+#[test]
+fn singular_variable() {
+    let mut builder = RouterBuilder::<&str>::new();
+    route!(builder; r"^/test/(.+)$"; "example"; "GET" => "Hello.");
+    let router = builder.build().unwrap();
+
+    let r_match = router.dispatch("GET", "/test/World.").unwrap();
+    let param = r_match.params().get("example").unwrap();
+
+    assert_eq!(*param, "World.");
+    assert_eq!(*r_match.handler(), "Hello.");
+
+    assert!(router.dispatch("POST", "/test/World.").is_none());
+    assert!(router.dispatch("GET", "/test-test").is_none());
+}
+
+#[test]
+fn multiple_methods() {
+    let mut builder = RouterBuilder::<&str>::new();
+    route!(builder; r"^/test$";; "GET" => "Hello.", "POST" => "World.");
+    let router = builder.build().unwrap();
+
+    assert_eq!(*router.dispatch("GET", "/test").unwrap().handler(), "Hello.");
+    assert_eq!(*router.dispatch("POST", "/test").unwrap().handler(), "World.");
+
+    assert!(router.dispatch("PUT", "/test").is_none());
+
+    assert!(router.dispatch("GET", "/test-test").is_none());
+    assert!(router.dispatch("POST", "/test-test").is_none());
 }
